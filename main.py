396ebc693e0eee60155de97726006b7f8e54a95e
charliebrown1221 <chaimbrown1@yahoo.com>
2022-09-14 15:06:23 -0400

    created class Map with function get_map and tested

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 import pygame as pg
 import sys
 from settings import *
-
+from map import *
 
 class Game:
     def __init__(self):
         pg.init()
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
+        self.new_game()
         
     def new_game(self):
-        pass
+        self.map =Map(self)
     
     
     def update(self):
         pg.display.flip()
         self.clock.tick(FPS)
-        pg.display.set_caption(f'{self.clock.get_fps() :.f}')    
+        pg.display.set_caption(f'{self.clock.get_fps() :.1f}')    
     
     
     def draw(self):
         self.screen.fill('black')
+        self.map.draw()
     
     def check_events(self):
-        for event in pg.events.get():
-            if event.type == pg.QUIT OR (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
                 PG.quit()  
                 sys.exit() 
         
@@ -33,4 +35,8 @@ class Game:
         while True:
             self.check_events()
             self.update()
-            self.draw()        
+            self.draw()  
+            
+if __name__ == '__main__':
+    game = Game()
+    game.run()      
