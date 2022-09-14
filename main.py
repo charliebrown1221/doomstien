5c357ab1d667ab2e0122ddc2e2bb9f25e781f8b4
charliebrown1221 <chaimbrown1@yahoo.com>
2022-09-14 14:35:16 -0400

    created class Game with functions new_game, update, draw,check_events and run

---
+++ main.py
@@ -0,0 +1,36 @@
+import pygame as pg
+import sys
+from settings import *
+
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode(RES)
+        self.clock = pg.time.Clock()
+        
+    def new_game(self):
+        pass
+    
+    
+    def update(self):
+        pg.display.flip()
+        self.clock.tick(FPS)
+        pg.display.set_caption(f'{self.clock.get_fps() :.f}')    
+    
+    
+    def draw(self):
+        self.screen.fill('black')
+    
+    def check_events(self):
+        for event in pg.events.get():
+            if event.type == pg.QUIT OR (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                PG.quit()  
+                sys.exit() 
+        
+    
+    def run(self):
+        while True:
+            self.check_events()
+            self.update()
+            self.draw()        
